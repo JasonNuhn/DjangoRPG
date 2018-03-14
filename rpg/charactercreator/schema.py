5ecd78dc4e3d305baa9613d696cea0fed271eb75
Jason Nuhn <jason.a.nuhn@gmail.com>
2018-03-14 15:43:45 -0400

    schema set up with item

--- rpg/charactercreator/schema.py
+++ rpg/charactercreator/schema.py
@@ -1,6 +1,11 @@
 from graphene_django import DjangoObjectType
 import graphene
 from .models import Character as CharacterModel
+from .models import Item as ItemModel
+
+class Item(DjangoObjectType):
+    class Meta:
+        model = ItemModel
 
 class Character(DjangoObjectType):
     class Meta:
@@ -8,8 +13,12 @@ class Character(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     characters = graphene.List(Character)
+    inventory = graphene.List(Item)
 
     def resolve_characters(self, info):
         return CharacterModel.objects.all()
+
+    def resolve_items(self, info):
+        return ItemModel.objects.all()
 
 schema = graphene.Schema(query=Query)
