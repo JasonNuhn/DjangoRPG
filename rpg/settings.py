5f10281c671a4c3a01dc486d8d4eea7011666102
Jason Nuhn <jason.a.nuhn@gmail.com>
2018-03-16 15:20:34 -0400

    updated work

--- rpg/settings.py
+++ rpg/settings.py
@@ -104,20 +104,20 @@ GRAPHENE = {
 
 WSGI_APPLICATION = 'rpg.wsgi.application'
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
